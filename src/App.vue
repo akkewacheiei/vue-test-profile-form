5b95179cd7df5aeb1f1893848455215440412974
Broy <akkewach.yodsomboon@gmail.com>
2024-07-09 01:14:26 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,122 @@
 <script setup>
-import { reactive } from "vue";
+import { ref, computed, onMounted, watch } from "vue";
 
-const gendersList = ["ชาย", "หญิง", "ไม่ระบุ"];
-const interestsList = ["หนังสือ", "กีฬา", "การเมือง", "เกม", "เขียนเว็บ"];
+const isLoading = ref(false);
+const isUpdated = ref(false);
+const isValid = ref(true);
 
-const formData = reactive({ interests: [] });
+const firstname = ref("");
+const lastname = ref("");
+const email = ref("");
+const errors = ref({});
 
-const submitForm = () => {
-  console.log("submitForm !!");
-  console.log("formData :", formData);
-  console.log("interests :", formData.interests.join());
+const fullname = computed(() => {
+  return `${firstname.value} ${lastname.value}`;
+});
+
+onMounted(() => {
+  firstname.value = "test";
+  lastname.value = "นามสกุล";
+  email.value = "test@mail.com";
+});
+
+const updateProfile = async () => {
+  isLoading.value = true;
+  // จำลองการส่ง API ออกมา
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  isLoading.value = false;
+  isUpdated.value = true;
 };
+
+const validateName = (name) => {
+  const re = /\d/;
+  return !re.test(name);
+};
+
+const validateEmail = (email) => {
+  return email.includes("@");
+};
+
+watch([firstname, lastname, email], () => {
+  isValid.value = true;
+  isUpdated.value = false;
+  errors.value = {};
+
+  if (!validateName(firstname.value)) {
+    isValid.value = false;
+    errors.value.firstname = "ชื่อจริงไม่ถูกต้อง";
+  }
+  if (!validateName(lastname.value)) {
+    isValid.value = false;
+    errors.value.lastname = "นามสกุลไม่ถูกต้อง";
+  }
+  if (!validateEmail(email.value)) {
+    isValid.value = false;
+    errors.value.email = "อีเมลไม่ถูกต้อง";
+  }
+});
 </script>
 
 <template>
-  <div>
+  <div class="container">
     <div>
-      <div>Firstname</div>
-      <input type="text" name="firstname" v-model="formData.firstname" />
+      <div>Fullname: {{ fullname }}</div>
+      <div>email: {{ email }}</div>
     </div>
     <div>
-      <div>Lastname</div>
-      <input type="text" name="lastname" v-model="formData.lastname" />
+      <div>Firstname:</div>
+      <input type="text" v-model="firstname" />
+      <div>{{ errors.firstname }}</div>
+
     </div>
     <div>
-      <div>Gender</div>
-      <div v-for="gender in gendersList">
-        <input
-          type="radio"
-          name="gender"
-          :value="gender"
-          v-model="formData.gender"
-        />
-        {{ gender }}
-      </div>
+      <div>Lastname:</div>
+      <input type="text" v-model="lastname" />
+      <div>{{ errors.lastname }}</div>
+
     </div>
     <div>
-      <div>Interests</div>
-      <div v-for="interest in interestsList">
-        <input
-          type="checkbox"
-          name="interest"
-          :value="interest"
-          v-model="formData.interests"
-        />
-        {{ interest }}
-      </div>
+      <div>Email:</div>
+      <input type="text" v-model="email" />
+      <div>{{ errors.email }}</div>
     </div>
-    <div>
-      <div>Description</div>
-      <textarea v-model="formData.description"></textarea>
-    </div>
-    <button @click="submitForm()">ส่ง Form</button>
+    <div v-if="isLoading" class="loading">Loading...</div>
+    <button :disabled="!isValid" class="button" @click="updateProfile()">
+      Update profile
+    </button>
+    <div v-if="isUpdated">Profile update เรียบร้อย</div>
   </div>
 </template>
+
+<style>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  max-width: 320px;
+  margin: 0 auto;
+}
+
+.container > div {
+  width: 100%;
+}
+
+.button {
+  margin-top: 20px;
+  width: 100%;
+  height: 24px;
+}
+
+.loading {
+  background-color: aliceblue;
+  width: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+  margin: 10px 0;
+}
+
+input {
+  width: 100%;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, onMounted } from "vue";
+const count = ref(0);
+const message = ref("");
+
+const increaseCounter = () => {
+  count.value += 1;
+};
+
+const props = defineProps({
+  messageText: String,
+  changeMessage: Function,
+});
+
+onMounted(() => {
+  console.log("on mounted at Counter.vue");
+});
+</script>
+
+<template>
+  <div>Hello {{ messageText }}</div>
+  <div>
+    <input type="text" v-model="message" />
+    <button @click="changeMessage(message)">Change message from counter</button>
+  </div>
+  <button @click="increaseCounter">Click {{ count }}</button>
+</template>
